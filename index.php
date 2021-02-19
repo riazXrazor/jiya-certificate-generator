2c714045913ffea69da37e2e9dc9bd7861af823d
Riaz Laskar <riazcool77@gmail.com>
2021-02-19 19:26:29 +0530

    new updated

--- index.php
+++ index.php
@@ -7,7 +7,8 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-
+    <link id="bsdp-css" href="https://unpkg.com/bootstrap-datepicker@1.9.0/dist/css/bootstrap-datepicker3.min.css" rel="stylesheet">
+    
     <title>JIYA Certificate Generator</title>
   </head>
   <body>
@@ -34,7 +35,7 @@
       </select>
       </div>
       <div class="form-group col-sm-12 col-md-4">
-       <input class="form-control" type="date" name="cdate" />
+       <input placeholder="Date for Certificate" class="form-control datepicker" type="text" name="cdate" />
       </div>
       </div>
       <button type="submit" name="generate" class="btn btn-primary">Generate</button>
@@ -182,5 +183,13 @@
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+    <script src="https://unpkg.com/bootstrap-datepicker@1.9.0/dist/js/bootstrap-datepicker.min.js"></script>
+    <script>
+      $(function(){
+        $('.datepicker').datepicker({
+          format: 'yyyy-mm-dd',
+        });
+      })
+    </script>
   </body>
 </html>
